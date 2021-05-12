87eebee7839b584962e7f88dffcaedf64d86ee71
Julien <cefadrom1@gmail.com>
2021-05-12 18:17:48 +0200

    Added a tag to center elements

---
+++ shop/src/lib/Center.svelte
@@ -0,0 +1,22 @@
+<style>
+    .container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        margin: auto;
+        min-height: calc(100vh - var(--header-height) - var(--spacing) * 2);
+    }
+
+    @media all and (min-width: 768px) {
+        .container {
+            min-height: calc(100vh - var(--header-height) * 3 - var(--spacing) * 2);
+        }
+    }
+</style>
+
+
+<div class="container">
+    <slot/>
+</div>
