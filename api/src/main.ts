2b919942ccd2de8acb4b25a6bcbc1b53b14c1e78
cefadrom <cefadrom1@gmail.com>
2021-05-13 20:22:26 +0200

    Moved the API from a subdomain to the same domain

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('v1');
+    app.setGlobalPrefix('api/v1');
 
     app.useGlobalPipes(
         new ValidationPipe({
