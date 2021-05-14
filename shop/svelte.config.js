c317a810c1aba49f70a44de209758495ab88c586
cefadrom <cefadrom1@gmail.com>
2021-05-14 14:21:22 +0200

    Updated svelte kit and deps

---
+++ shop/svelte.config.js
@@ -0,0 +1,13 @@
+import sveltePreprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: sveltePreprocess(),
+    kit: {
+        adapter: node(),
+        target: '#svelte',
+    }
+};
+
+export default config;
