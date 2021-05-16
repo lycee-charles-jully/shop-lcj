0811845a24351d4f0c7356257e4fa7376df35477
Julien <cefadrom1@gmail.com>
2021-05-16 19:44:53 +0200

    Added the ability to have a thumbnail

--- admin/src/lib/Item.svelte
+++ admin/src/lib/Item.svelte
@@ -1,8 +1,16 @@
 <script>
+    import { imageUrl } from '$lib/image-url';
+
     export let href;
+    export let thumb;
 </script>
 
 
-<a class="block w-full bg-white p-4 rounded-md my-2" {href}>
-    <slot/>
+<a class="block w-full bg-white rounded-md my-2 h-14 flex justify-start" {href}>
+    {#if thumb}
+        <img src={imageUrl(thumb)} class="h-14 w-14 rounded-l-md"/>
+    {/if}
+    <div class="p-4">
+        <slot/>
+    </div>
 </a>
