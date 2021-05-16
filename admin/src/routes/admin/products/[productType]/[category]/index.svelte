c7990ff6f2549a892daa974f72c41d5c8ff1f41e
Julien <cefadrom1@gmail.com>
2021-05-16 22:10:22 +0200

    Added the products display

---
+++ admin/src/routes/admin/products/[productType]/[category]/index.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+    import { REMOTE_ENDPOINT } from '$lib/api-url';
+    import Item from '$lib/Item.svelte';
+
+    const productTypeID: string = $page.params.productType;
+    const categorySlug: string = $page.params.category;
+
+    let products = [];
+
+    let caregory: string, productType: string;
+
+    onMount(() => {
+        fetch(`${REMOTE_ENDPOINT}/v1/product?limit=20&category=${categorySlug}`)
+            .then(res => res.json())
+            .then(p => products = p);
+        fetch(`${REMOTE_ENDPOINT}/v1/category`)
+            .then(res => res.json())
+            .then(categories => {
+                const { name, productType: { name: productTypeName } } = categories.find(c => c.slug === categorySlug);
+                caregory = name;
+                productType = productTypeName;
+            });
+    });
+</script>
+
+
+<h1 class="text-2xl mb-4">
+    {#if caregory && productType}
+        <a href="/admin/products">{productType}</a> {'>'}
+        <a href="/admin/products/{productTypeID}">{caregory}</a> {'>'}
+        produits
+    {:else}
+        Produits
+    {/if}
+</h1>
+
+{#each products as product}
+    <Item href="/admin/products/{productTypeID}/{categorySlug}/{product.slug}"
+          thumb={product.coverImageUrl}>
+        {product.name}
+    </Item>
+{/each}
+
+<a class="block w-full bg-white p-4 rounded-md mt-6" href="/admin/products/{productTypeID}/{categorySlug}/new">
+    Ajouter un produit
+</a>
