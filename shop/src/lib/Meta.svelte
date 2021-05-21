5531a1202ff456df346787e94efeee2fbbcc4051
Julien <cefadrom1@gmail.com>
2021-05-21 23:29:10 +0200

    Reworked the title display

--- shop/src/lib/Meta.svelte
+++ shop/src/lib/Meta.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    export let title = 'Shop LCJ';
+    export let title;
+    export let nomarker = false;
+
+    const defaultTitle = 'Shop LCJ';
 </script>
 
 <svelte:head>
-    <title>{title}</title>
+    <title>{title || defaultTitle}{title && !nomarker ? ' - Shop LCJ' : ''}</title>
 </svelte:head>
