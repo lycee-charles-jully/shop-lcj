f48ee34ce7d2faec621563116fd431be71864061
Julien <cefadrom1@gmail.com>
2021-05-25 22:44:13 +0200

    Added the ordering and success steps with error handling

---
+++ shop/src/lib/order/OrderSucceed.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import Button from '$lib/Button.svelte';
+    import { onDestroy, onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+
+    let endingTime: number;
+    let intervalID: number;
+
+    let h: number = 48;
+    let m: number = 0;
+    let s: number = 0;
+
+    // TODO: easter egg on 0
+    onMount(() => {
+        endingTime = Date.now() + 86400 * 1000 * 2; // ends in 48h
+        updateRemainingTime();
+        intervalID = setInterval(updateRemainingTime, 1000) as number;
+    });
+
+    onDestroy(() => {
+        clearInterval(intervalID);
+    });
+
+    function updateRemainingTime() {
+        let remainingTime = (endingTime - Date.now()) / 1000;
+        h = Math.floor(remainingTime / 3600);
+        m = Math.floor((remainingTime - h * 3600) / 60);
+        s = Math.floor(remainingTime - h * 3600 - m * 60);
+    }
+
+    function toDoubleDigits(time: number = 0) {
+        const t = time.toString();
+        return t.length === 2 ? t : `0${t}`;
+    }
+</script>
+
+
+<h3>Yay, votre commande a bien été effectuée !</h3>
+<p>
+    Vous pouvez désormais prendre un coktail et vous relaxer pendant que nous nous occupons de tout !<br/>
+    Vous pouvez à tout moment suivre l'avancement de votre commande ou l'annuler dans l'onglet
+    <a href="/account" on:click|preventDefault={() => goto('/account', { replaceState: true })}>Compte</a>.
+    À noter que vous disposez de {toDoubleDigits(h)}h {toDoubleDigits(m)}min {toDoubleDigits(s)}s pour l'annuler.
+    Alors si vous n'êtes pas sûr, décidez vous !
+</p>
+
+
+<Button on:click={() => goto('/', { replaceState: true })}>
+    Retour à l'accueil
+</Button>
--- shop/src/routes/order.svelte
+++ shop/src/routes/order.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import type { CartItemPopulated } from '$types/cart';
     import type { Recommendation } from '$types/recommendation';
+    import type { User } from '$types/user';
     import CartConfirmation from '$lib/order/CartConfirmation.svelte';
-    import Eula from '../lib/order/Eula.svelte';
+    import Eula from '$lib/order/Eula.svelte';
+    import OrderSucceed from '$lib/order/OrderSucceed.svelte';
     import { onMount } from 'svelte';
     import { REMOTE_ENDPOINT } from '$lib/api-url';
     import Meta from '$lib/Meta.svelte';
     import Recommendations from '$lib/order/Recommendations.svelte';
+    import { session } from '$app/stores';
 
-    let step: 'LOADING' | 'RECOMMENDATIONS' | 'CONFIRM_ITEMS' | 'EULA' | 'CONFIRMATION' = 'LOADING';
+    let step: 'LOADING' | 'RECOMMENDATIONS' | 'CONFIRM_ITEMS' | 'EULA' | 'ORDERING' | 'SUCCESS' | 'ERROR' = 'LOADING';
 
     let recommendations: Recommendation[];
     let cart: CartItemPopulated[];
@@ -31,6 +34,37 @@
             });
     });
 
+
+    let error: string | null = null;
+
+    function setError(e: Error) {
+        step = 'ERROR';
+        error = e.message;
+    }
+
+
+    async function order() {
+        step = 'ORDERING';
+        await fetch(`${REMOTE_ENDPOINT}/v1/order/from-cart`, {
+            method: 'POST',
+            body: JSON.stringify({
+                recommendations: validatedRecommendations.map(r => ({ count: r.count, product: r.product._id })),
+            }),
+            credentials: 'include',
+            headers: [
+                [ 'Content-Type', 'application/json' ],
+            ],
+        })
+            .then(async res => ({ res, data: await res.json() }))
+            .then(({ res, data }) => {
+                if (!res.ok)
+                    throw new Error(data.message || JSON.stringify(data));
+                ($session.user as User).cart = [];
+                step = 'SUCCESS';
+            })
+            .catch(e => setError(e));
+    }
+
 </script>
 
 
@@ -50,5 +84,13 @@
     <CartConfirmation {cart} {validatedRecommendations}
                       on:nextstep={() => step = 'EULA'}/>
 {:else if step === 'EULA'}
-    <Eula on:nextstep={() => step = 'CONFIRMATION'}/>
+    <Eula on:nextstep={order}/>
+{:else if step === 'ORDERING'}
+    Commande en cours...
+{:else if step === 'SUCCESS'}
+    <OrderSucceed/>
+{:else if step === 'ERROR'}
+    <h3 class="error-message">Une erreur est survenue</h3>
+    <p class="error-message">{error}</p>
+    <a href="/cart">Retour au panier</a>
 {/if}
