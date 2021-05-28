c908bf59e901ac120b9d39374733aaeedaeaf29e
Julien <cefadrom1@gmail.com>
2021-05-28 23:05:38 +0200

    Made the color reactive and unified the structure

--- admin/src/lib/OrderStatus.svelte
+++ admin/src/lib/OrderStatus.svelte
@@ -3,7 +3,8 @@
 
     export let status: 'WAITING_FOR_ACCEPTATION' | 'PREPARATING' | 'DELIVERING' | 'COMPLETED' | 'USER_CANCELLED' | 'ADMIN_CANCELLED';
 
-    let color = statusColors.get(status);
+    let color;
+    $: color = statusColors.get(status);
 </script>
 
 
@@ -14,14 +15,16 @@
 </style>
 
 
-{#if status === 'WAITING_FOR_ACCEPTATION'}
-    <div class="bg-{color}">À confirmer</div>
-{:else if status === 'PREPARATING'}
-    <div class="bg-{color}">Préparation</div>
-{:else if status === 'DELIVERING'}
-    <div class="bg-{color}">Livraison</div>
-{:else if status === 'COMPLETED'}
-    <div class="bg-{color}">Complétée</div>
-{:else if status === 'USER_CANCELLED' || status === 'ADMIN_CANCELLED'}
-    <div class="bg-{color}">Annulée</div>
-{/if}
+<div class="bg-{color}">
+    {#if status === 'WAITING_FOR_ACCEPTATION'}
+        À confirmer
+    {:else if status === 'PREPARATING'}
+        Préparation
+    {:else if status === 'DELIVERING'}
+        Livraison
+    {:else if status === 'COMPLETED'}
+        Complétée
+    {:else if status === 'USER_CANCELLED' || status === 'ADMIN_CANCELLED'}
+        Annulée
+    {/if}
+</div>
