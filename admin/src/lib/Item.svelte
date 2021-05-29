e7649f8491d736622b1cf45ab50801c4e708e810
Julien <cefadrom1@gmail.com>
2021-05-29 15:37:45 +0200

    Added the statically CDN for the admin panel

--- admin/src/lib/Item.svelte
+++ admin/src/lib/Item.svelte
@@ -9,7 +9,7 @@
 
 <a class="block w-full bg-white rounded-md my-2 h-14 flex justify-start" {href} {target}>
     {#if thumb}
-        <img src={imageUrl(thumb)} class="h-14 w-14 rounded-l-md"/>
+        <img src={imageUrl(thumb, 200)} class="h-14 w-14 rounded-l-md"/>
     {/if}
     <div class="p-4 flex-1">
         <slot/>
