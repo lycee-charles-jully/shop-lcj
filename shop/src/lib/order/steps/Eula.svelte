74ed3993b7f03e292b2929dc4198267cb7a6f1b9
Julien <cefadrom1@gmail.com>
2021-06-03 10:50:07 +0200

    Updated the EULA logic and text

--- shop/src/lib/order/steps/Eula.svelte
+++ shop/src/lib/order/steps/Eula.svelte
@@ -30,7 +30,7 @@
 </style>
 
 
-<h3>Conditions générales d'utilisation</h3>
+<h3>Conditions générales de vente</h3>
 
 <h4>Clause n° 1 : Objet</h4>
 <p>
@@ -41,11 +41,12 @@
 </p>
 
 <h4>Clause n° 2 : Prix</h4>
-Les prix des marchandises vendues sont ceux en vigueur au jour de la
-prise de commande. Ils sont libellés en euros et calculés hors taxes.
-La Maison des lycéens s'accorde le droit de modifier ses tarifs à tout
-moment. Toutefois, elle s'engage à facturer les marchandises commandées
-aux prix indiqués lors de l'enregistrement de la commande.
+<p>
+    Les prix des marchandises vendues sont ceux en vigueur au jour de la prise de commande. Ils sont libellés en euros
+    et calculés toutes taxes comprises. La Maison des lycéens s'accorde le droit de modifier ses tarifs à tout moment.
+    Toutefois, elle s'engage à facturer les marchandises commandées aux prix indiqués lors de l'enregistrement de la
+    commande.
+</p>
 
 <h4>Clause n° 3 : Modalités de paiement</h4>
 <p class="no-bm">
@@ -53,7 +54,7 @@ aux prix indiqués lors de l'enregistrement de la commande.
 </p>
 <ul class="no-mt">
     <li>soit par liquide</li>
-    <li>soit par chèque</li>
+    <li>soit par chèque, à l'ordre de la Maison des lycéens</li>
 </ul>
 
 <h4>Clause n° 4 : Annulation de commande</h4>
@@ -69,14 +70,19 @@ aux prix indiqués lors de l'enregistrement de la commande.
     garanti.
 </p>
 
-<label>
-    <input bind:checked={accepted} type="checkbox">
-    J'ai lu et j'accepte les conditions
-</label>
+<form on:submit|preventDefault={() => dispatch('nextstep')}>
 
-<Button disabled={!accepted} on:click={() => accepted && dispatch('nextstep')}>
-    Commander
-</Button>
+    <label class="big-mb">
+        <input bind:checked={accepted} required type="checkbox">
+        J'ai lu et j'accepte les conditions
+    </label>
+
+    <Button disabled={!accepted}>
+        Commander
+    </Button>
+
+</form>
+
 
 <Button nomargin on:click={() => showDisallowPopup = true} type="secondary">
     Refuser
@@ -84,9 +90,9 @@ aux prix indiqués lors de l'enregistrement de la commande.
 
 {#if showDisallowPopup}
     <Popup on:close={() => showDisallowPopup = false} backdrop>
-        <h1>Refuser les CGU</h1>
-        <p>Vous devez accepter les conditions générales d'utilisation afin d'effectuer une commande.</p>
-        <Button on:click={() => showDisallowPopup = false}>Retour</Button>
+        <h1>Refuser les CGV</h1>
+        <p>Vous devez accepter les conditions générales de vente afin d'effectuer une commande.</p>
+        <Button on:click={() => showDisallowPopup = false}>Accepter</Button>
         <Button type="secondary" on:click={() => goto('/cart')} nomargin>Refuser</Button>
     </Popup>
 {/if}
