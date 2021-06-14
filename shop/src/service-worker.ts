6c92d2c8661ca61312569c5db07c6b1a933683d3
Julien <cefadrom1@gmail.com>
2021-06-14 23:26:55 +0200

    Added an offline page

---
+++ shop/src/service-worker.ts
@@ -0,0 +1,39 @@
+const VERSION = 'v1';
+
+
+self.addEventListener('install', (evt: ExtendableEvent) => {
+    // @ts-ignore
+    self.skipWaiting();
+    evt.waitUntil((async () => {
+        const cache = await caches.open(VERSION);
+        await cache.add('/offline.min.html');
+    })());
+});
+
+
+self.addEventListener('activate', (evt: ExtendableEvent) => {
+    // @ts-ignore
+    clients.claim();
+    evt.waitUntil((async () => {
+        await Promise.all((await caches.keys()).map(key => {
+            if (!key.includes(VERSION))
+                return caches.delete(key);
+        }));
+    })());
+});
+
+
+self.addEventListener('fetch', (evt: FetchEvent) => {
+    if (evt.request.mode === 'navigate')
+        evt.respondWith((async () => {
+            try {
+                return (await evt.preloadResponse) || (await fetch(evt.request));
+            } catch {
+                const cache = await caches.open(VERSION);
+                return await cache.match('/offline.min.html');
+            }
+        })());
+});
+
+
+export {};
