9e709db38303bc00f167bd754944ebee7757f45d
Julien <cefadrom1@gmail.com>
2021-06-21 22:20:25 +0200

    Added a button to delete a product

--- admin/src/routes/admin/products/[productType]/[category]/[product]/index.svelte
+++ admin/src/routes/admin/products/[productType]/[category]/[product]/index.svelte
@@ -41,6 +41,7 @@
     import dayjs from 'dayjs';
     import { goto } from '$app/navigation';
     import { REMOTE_ENDPOINT } from '$lib/api-url';
+    import Popup from '$lib/Popup.svelte';
 
     export let product;
     export let error: string | null = null;
@@ -75,6 +76,41 @@
             })
             .finally(() => {
                 loading = false;
+            });
+    }
+
+
+    let showDeletePopup = false;
+    let deleteInputName = '';
+    let deletingProduct = false;
+
+    function promptDeletePopup() {
+        deleteInputName = '';
+        showDeletePopup = true;
+    }
+
+    function deleteProduct() {
+        if (deletingProduct || deleteInputName !== product.name)
+            return;
+        deletingProduct = true;
+        fetch(`${REMOTE_ENDPOINT}/v1/product/${product._id}`, {
+            method: 'DELETE',
+            body: JSON.stringify({ name: deleteInputName }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(async res => ({ res, data: await res.json() }))
+            .then(({ res, data }) => {
+                if (!res.ok)
+                    throw new Error(data.message || JSON.stringify(data));
+                goto('./', { replaceState: true });
+            })
+            .catch(err => {
+                console.error(err);
+                error = err.message || err;
+                showDeletePopup = false;
+                deletingProduct = false;
             });
     }
 </script>
@@ -125,10 +161,40 @@
                 Modifier
             </button>
 
-            <button class="{product.available ? 'bg-red-500' : 'bg-green-500'} text-white w-full px-4 py-2 rounded"
+            <button class="{product.available ? 'bg-red-500' : 'bg-green-500'} text-white w-full px-4 py-2 rounded mb-1"
                     on:click={toggleProductAvailability}>
                 Rendre {product.available ? 'in' : ''}disponible
             </button>
+
+            <button class="bg-red-600 text-white w-full px-4 py-2 rounded" on:click={promptDeletePopup}>
+                Supprimer
+            </button>
         </div>
     </div>
+{/if}
+
+
+{#if showDeletePopup}
+    <Popup title="Supprimer le produit" on:close={() => showDeletePopup = false}>
+        <p class="mb-2">Êtes-vous sur de vouloir supprimer <b>{product.name}</b> ?</p>
+        Veuillez prendre en compte ces conséquences :
+        <ul class="mb-2 list-disc list-inside">
+            <li>Le produit sera supprimé définitivement, sans retour en arrière possible</li>
+            <li>Les images seront supprimées</li>
+            <li>Le produit sera retiré du panier de tous les utilisateurs</li>
+        </ul>
+        <p class="mb-2">À noter qu'il est impossible de supprimer un produit si il a été commandé au moins une fois.</p>
+        <p class="mb-1">
+            Afin de confirmer la suppression, veuillez écrire <b>{product.name}</b> dans l'encadré ci-dessous :
+        </p>
+        <input type="text"
+               class="input bg-gray-100 w-full mb-2"
+               bind:value={deleteInputName}
+               on:paste|preventDefault on:dragenter|preventDefault on:dragover|preventDefault on:drop|preventDefault/>
+        <button class="btn bg-red-600 text-white w-full"
+                on:click={deleteProduct}
+                disabled={deleteInputName !== product.name || deletingProduct}>
+            Supprimer
+        </button>
+    </Popup>
 {/if}
