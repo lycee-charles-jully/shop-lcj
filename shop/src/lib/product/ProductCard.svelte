12e27c572ef67c100c81d2f6b157dca3e1bf0ed9
Julien <cefadrom1@gmail.com>
2021-12-23 20:49:55 +0100

    feat: stock count display on product cards

--- shop/src/lib/product/ProductCard.svelte
+++ shop/src/lib/product/ProductCard.svelte
@@ -29,6 +29,7 @@
 
 <style>
     .card {
+        position: relative;
         color: var(--black);
         background-color: var(--white);
         max-width: 250px;
@@ -39,10 +40,40 @@
         justify-self: center;
     }
 
+    .stock-display {
+        --display-background: rgba(0, 0, 0, .4);
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: var(--display-background);
+        z-index: 2;
+        border-radius: var(--round) var(--round) 0 0;
+        color: white;
+        text-align: center;
+        padding-top: 2px;
+        font-weight: bold;
+        text-shadow: 0 0 1px black;
+    }
+
+    .stock-display::after {
+        content: "";
+        position: absolute;
+        height: 5px;
+        width: 100%;
+        top: 100%;
+        left: 0;
+        background: linear-gradient(var(--display-background), transparent);
+    }
+
     .cover {
         width: 100%;
         border-radius: var(--round) var(--round) 0 0;
         padding-top: 100%;
+    }
+
+    .out-of-stock {
+        filter: grayscale(.8);
     }
 
     .body {
@@ -67,26 +98,41 @@
 
 
 <a class="card" href="/product/{product.slug}" on:click={goToProductPage}>
-    <picture class="product-img cover" use:imgload>
+
+    {#if product.stockCount !== null && product.stockCount <= 5}
+        <div class="stock-display">
+            {#if product.stockCount <= 0}
+                Ruptude de stock
+            {:else}
+                {product.stockCount} restant{product.stockCount > 1 ? 's' : ''}
+            {/if}
+        </div>
+    {/if}
+
+    <picture class="product-img cover" class:out-of-stock={product.stockCount === 0} use:imgload>
         <img alt={product.name} height="200" loading="lazy" src={imageUrl(product.coverImageUrl, 200)} width="200"/>
     </picture>
+
     <div class="body">
         <span class="name">{product.name}</span>
         <div class="bottom-bar">
             <span class="text-gradient">{currencyFormat(product.price)}</span>
-            {#if $session.user?.cart?.find(i => i.product === product._id)}
-                <a href="/cart">
-                    <img alt="v" src="/icons/cart-highlight.svg" width="20" height="20"/>
-                </a>
-            {:else}
-                <img alt="+"
-                     src="/icons/add-cart.svg"
-                     width="20" height="20"
-                     on:click={handleQuickAdd}
-                     class="add-cart-btn"/>
+            {#if product.stockCount === null || product.stockCount > 0}
+                {#if $session.user?.cart?.find(i => i.product === product._id)}
+                    <a href="/cart">
+                        <img alt="v" src="/icons/cart-highlight.svg" width="20" height="20"/>
+                    </a>
+                {:else}
+                    <img alt="+"
+                         src="/icons/add-cart.svg"
+                         width="20" height="20"
+                         on:click={handleQuickAdd}
+                         class="add-cart-btn"/>
+                {/if}
             {/if}
         </div>
     </div>
+
 </a>
 
 
